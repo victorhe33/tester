e79725689d3870b9759487843795550ca1c22d44
Victor <94798862+victorhe33@users.noreply.github.com>
2022-06-19 21:39:33 -0400

    OOP playground

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-const container = document.querySelector('container');
+const myLibrary = [{ title: 'The Bible', author: 'God', pages: 1000, read: 'read' }];
 
-const unsplash = fetch('https://picsum.photos/list')
-  .then((response) => response.json())
-  .then((images) =>{
-    return images;
-  });
+class Book {
+  constructor (title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-const summonImage = async () => {
-  const a = await unsplash;
-  const randomNum = Math.floor(a.length * Math.random());
-  const chosenOne = a[randomNum].post_url;
-  
-  return chosenOne;
-};
+  info() {
+    console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
+  }
 
-summonImage();
+  toggle() {
+    this.read === 'read' ? this.read = 'not read' : this.read = 'read';
+  }
+}
+
+function addBookToLibrary(...input) {
+  const newBook = new Book(...input);
+  myLibrary.push(newBook);
+}
+
+function displayBooks() {
+  myLibrary.forEach(book => {
+    //display in DOM
+    //maybe add a card in a flexbox container?
+    console.log(book);
+  })
+}
+
+displayBooks();
+
