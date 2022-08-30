c643b0b060054848789951ab6371c01fa7f442cd
Victor <94798862+victorhe33@users.noreply.github.com>
2022-08-30 17:52:39 -0400

    INIT Binary tree level order traversal

---
+++ Algos/Leetcodes/BTLevelOrder.js
@@ -0,0 +1,26 @@
+https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ * 
+ *
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+ */
+
+/*
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+
+var levelOrder = function (root) {
+
+};
