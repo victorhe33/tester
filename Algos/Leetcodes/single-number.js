56113309571b05996b9b8b1b4293740499ee5f4a
Victor <94798862+victorhe33@users.noreply.github.com>
2022-09-12 16:13:10 -0400

    completed single num - had to google

--- Algos/Leetcodes/single-number.js
+++ Algos/Leetcodes/single-number.js
@@ -1,2 +1,17 @@
 //BIT MANIPULATION
 //https://leetcode.com/problems/single-number/
+
+/*
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// time - O(n)
+// space O(1)
+
+//I had to refer to a website.
+//https://levelup.gitconnected.com/single-number-algorithm-7384f74422
+//XOR operator
+var singleNumber = function (nums) {
+  return nums.reduce((accumulator, value) => accumulator ^ value)
+};
