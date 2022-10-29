5ad954b123ceb020f65773ba6d1f7e6abfe59ed8
Victor <94798862+victorhe33@users.noreply.github.com>
2022-10-29 11:50:15 -0400

    completed grad algo prac

--- Algos/grad.js
+++ Algos/grad.js
@@ -12,8 +12,34 @@
 // zeroToTwenty(10, 15, 20, 0, 5, 10, 15, 20, 0) // => [20, 30, 40, 0, 10, 30, 40, 0]
 
 function rotationSequence(callback, ...args) {
+  let index = 0;
+  let length = args.length
+  return function (...inputs) {
+    const result = [];
+    for (let i = 0; i < inputs.length; i++) {
+      //invalid entry handling
+      if (inputs[i] !== args[index]) return 'invalid input'; 
+      
+      //core logic - push result of callback on input into result array;
+      result.push(callback(inputs[i]));
+      
+      //increment index
+      index++;
+      if ((index) > length - 1) index = 0;
+    }
 
+    return result;
+  }
 }
+
+// const multiplyBy2 = (num) => num * 2;
+// const zeroToTwenty = rotationSequence(multiplyBy2, 0, 5, 10, 15, 20);
+// console.log(zeroToTwenty(0, 5, 10)) // -> [0, 10, 20]
+// console.log(zeroToTwenty(20)) // -> 'invalid input'
+// console.log(zeroToTwenty(15, 20, 0, 5)) // -> [30, 40, 0, 10]
+// console.log(zeroToTwenty(10, 15, 20, 0, 5, 10, 15, 20, 0)) // => [20, 30, 40, 0, 10, 30, 40, 0]
+
+
 
 
 // Declare a function "stringLeveler", which takes two arguments: an array of strings and a number.
@@ -28,8 +54,44 @@ function rotationSequence(callback, ...args) {
 // console.log(creatures) // -> ['cat***', 'ferret', 'elepha', 'tyrann', 'minota'];
 
 function stringLeveler(array, num) {
+  let remainder = '';
+  for (let i = 0; i < array.length; i++){
+    const element = array[i];
+    const length = array[i].length;
+    console.log(element)
+    let substring = '';
 
+    for (let j = 0; j < num; j++) {
+      const letter = array[i][j]
+      // console.log(letter)
+      // If the string is longer than the passed-in number, remove characters from the end until it reaches the target length.
+      if (j <= length - 1) {
+        substring += letter;
+        // console.log(substring)
+      }
+      // If the string is shorter than the passed-in number, append "*" characters to the end until it reaches the target length.
+      if (j > length - 1) {
+        substring += '*';
+      }
+    }
+    //assign in place in array
+    array[i] = substring;
+
+    // It should return a string consisting of whichever characters have been deleted from the strings in the array. If none have been deleted, return an empty string.
+    if (length > num) {
+      for (let h = num; h < length; h++) {
+        // console.log(element[h])
+        remainder += element[h];
+      }
+    }
+  }
+
+  return remainder;
 }
+
+const creatures = ['cat', 'ferret', 'elephant', 'tyrannosaurus', 'minotaur'];
+console.log(stringLeveler(creatures, 6)) // -> 'ntosaurusur'
+console.log(creatures) // -> ['cat***', 'ferret', 'elepha', 'tyrann', 'minota'];
 
 
 // Declare a function "deepMap", which takes an array and a callback as arguments.
@@ -41,12 +103,26 @@ function stringLeveler(array, num) {
 // deepMap([1, 3, 5, [4, 10, [11], 12, [22, 8]]], multiplyBy2) // -> [2, 6, 10, [8, 20, [22], 24, [44, 16]]]]
 
 function deepMap(array, callback) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    // console.log(array[i])
+    //recursive call if element is a nested array;
+    if (Array.isArray(array[i])) {
+      result.push(deepMap(array[i], callback));
+    } else {
+      //other wise must be a primitive;
+      result.push(callback(array[i]));
+    }
+  }
 
+  return result;
 }
 
+const multiplyBy2 = (num) => num * 2;
+console.log(deepMap([1, 3, 5, [4, 10, [11], 12, [22, 8]]], multiplyBy2)); // -> [2, 6, 10, [8, 20, [22], 24, [44, 16]]]]
 
-module.exports = {
-  rotationSequence,
-  stringLeveler,
-  deepMap
-}
+// module.exports = {
+//   rotationSequence,
+//   stringLeveler,
+//   deepMap
+// }
