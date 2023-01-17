412ec8058a63ab68e225e64fc6a832a19fc8b352
Victor <94798862+victorhe33@users.noreply.github.com>
2023-01-16 23:55:50 -0500

    progress for 1/16/23 on python

--- Python/helloworld.py
+++ Python/helloworld.py
@@ -1,6 +1,25 @@
-patient_name = "John Smith"
-age = 20
-new_patient = True
+# patient_name = "John Smith"
+# age = 20
+# new_patient = True
 
-print(age)
+# print(age)1
 
+# 
+
+# my_name = input("What is your name? ")
+
+# print("Hello " + my_name)
+
+# 
+
+# birth_year = input("Enter your birth year: ")
+# age = 2020 - int(birth_year)
+# print(bool(age))
+
+# first = input("First: ")
+# second = input("Second: ")
+# sum = float(first) + float(second)
+# print("Sum: " + str(sum))
+
+course = 'Python for Beginners'
+print(course.upper())
