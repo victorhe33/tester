72e661166ea0cca398e50b1ee2a20b81f14a4db9
Victor <94798862+victorhe33@users.noreply.github.com>
2023-04-23 23:25:17 -0400

    add init commentParser, type testing

---
+++ Algos/commentParser.js
@@ -0,0 +1,11 @@
+/* Test program / 
+   int main()
+   {
+      // variable declaration 
+      int a, b, c;
+      / This is a test
+          multiline
+          comment for
+          testing */
+//a = b + c;
+//   }
---
+++ Examples/types.ts
@@ -0,0 +1,3 @@
+const array = [null, 1]
+
+console.log(array.filter(Boolean))
